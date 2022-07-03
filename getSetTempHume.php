03ef5b79ef8d077c63ed7246c344e0159fff34cc
vinicio <vinicio.naranjo15@gmail.com>
2022-07-03 20:41:59 +0200

    GSTHmodified para github

--- getSetTempHume.php
+++ getSetTempHume.php
@@ -1,84 +1,84 @@
 <?php
-$fp = fopen("temp.txt", 'r');
-$fh = fopen("humedad.txt", 'r');
+$fp = fopen("temperature.txt", 'r');
+$fh = fopen("humidity.txt", 'r');
 
 //Inicialitzo les dues variables
-$temp = "";
-$humedad = "";
+$temperature = "";
+$humidity = "";
 
-/*recuperar datos del fichero temp.txt */
+/*recuperar datos del fichero temperature$temperature.txt */
 if (!$fp) {
     echo '<p>No he pogut. Torna a introduir-la</p>';
 } else {
     //En cas que hagi pogut obrir bloquejo
     flock($fp, LOCK_EX);
 
-    //Vaig extraient les dades de temp
+    //Vaig extraient les dades de temperature$temperature
     while (!feof($fp)) {
-        $temp = $temp . fgets($fp, 1024);
+        $temperature = $temperature . fgets($fp, 1024);
     }
     //eliminar ultima linea en blanco
-    $temp = chop($temp, " ");
+    $temperature = chop($temperature, " ");
     flock($fp, LOCK_UN);
     fclose($fp);
 }
 
-/*recuperar datos del fichero humedad.txt */
+/*recuperar datos del fichero humidity$humidity.txt */
 if (!$fh) {
     echo '<p>No he pogut. Torna a introduir-la</p>';
 } else {
     //En cas que hagi pogut obrir bloquejo
     flock($fh, LOCK_EX);
 
-    //Vaig extraient les dades de temp
+    //Vaig extraient les dades de temperature$temperature
     while (!feof($fh)) {
-        $humedad = $humedad .  fgets($fh, 1024);
+        $humidity = $humidity .  fgets($fh, 1024);
     }
-    //eliminar la ultima coma en caso de que el fichero humedad tenga la ultima linea en blanco
-    $humedad = chop($humedad, " ");
+    //eliminar la ultima coma en caso de que el fichero humidity$humidity tenga la ultima linea en blanco
+    $humidity = chop($humidity, " ");
     flock($fh, LOCK_UN);
     fclose($fh);
 }
 
 /*tratamiento de errores del sensor, se resta dos ya que es chino y lee regular. La tolerancia del proximo sensor sera de 0.1%  */
-$temp = (float)$temp - 2;
-$humedad = (float)$humedad - 2;
+$temperature = (float)$temperature - 2;
+$humidity = (float)$humidity - 2;
 
 /* inyeccion en la base de datos */
 $llave = 0;
-inserta($llave, $temp, $humedad);
+inserta($llave, $temperature, $humidity);
 
-//Un cop llegit la dada i guardada enm la BD borro el que hi ha dins al fitxer temp.txt
-$fq = fopen("temp.txt", 'w+');
-$temp = "";
+//Un cop llegit la dada i guardada enm la BD borro el que hi ha dins al fitxer temperature$temperature.txt
+$fq = fopen("temperature$temperature.txt", 'w+');
+$temperature = "";
 if (!$fq) {
     echo '<p>No he pogut emmagatzemar el contingut de la comanda. Torna a introduir-la</p>';
 } else {
     //En cas que hagi pogut obrir bloquejo
     flock($fq, LOCK_EX);
 
-    fwrite($fq, $temp);
+    fwrite($fq, $temperature);
 
     flock($fq, LOCK_UN);
     fclose($fq);
 }
-//Un cop llegit la dada i guardada enm la BD borro el que hi ha dins al fitxer humedad.txt
-$fq2 = fopen("humedad.txt", 'w+');
-$humedad = "";
+//Un cop llegit la dada i guardada enm la BD borro el que hi ha dins al fitxer humidity$humidity.txt
+$fq2 = fopen("humidity$humidity.txt", 'w+');
+$humidity = "";
 if (!$fq2) {
     echo '<p>No he pogut emmagatzemar el contingut de la comanda. Torna a introduir-la</p>';
 } else {
     //En cas que hagi pogut obrir bloquejo
     flock($fq2, LOCK_EX);
 
-    fwrite($fq2, $humedad);
+    fwrite($fq2, $humidity);
 
     flock($fq2, LOCK_UN);
     fclose($fq2);
 }
 
 //funcion para insertar en la tabla tblsensores
-function inserta($llave,  $temp, $humedad)
+function inserta($llave,  $temperature, $humidity)
 {
     //base de datos
     $h = 'localhost';
@@ -86,8 +86,8 @@ function inserta($llave,  $temp, $humedad)
     //$u = 'pi';
     //$p = 'raspberry';
     $p = '';
-    //$b = 'temphumedad';
-    $b = 'temphumedad';
+    //$b = 'temperature$temperaturehumidity$humidity';
+    $b = 'temperature$temperaturehumidity$humidity';
     $ok = 1;
 
     $conn = mysqli_connect($h, $u, $p, $b);
@@ -97,7 +97,7 @@ function inserta($llave,  $temp, $humedad)
     }
     //insert en la base de datos
 
-    $sql = 'INSERT INTO tblsensores VALUES (' . $llave . ', ' . $temp . ', ' . $humedad . ');';
+    $sql = 'INSERT INTO tblsensores VALUES (' . $llave . ', ' . $temperature . ', ' . $humidity . ');';
     if (mysqli_query($conn, $sql)) {
         $res = array('val' => $ok);
         echo json_encode($res);
